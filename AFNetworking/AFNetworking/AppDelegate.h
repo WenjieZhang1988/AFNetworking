d7a2dfebfd820dac09d46b9f0a67e28582a20314
Kevin <Kevin.1988@foxmail.com>
2015-03-25 02:32:43 +0800

    常用网络框架

---
+++ AFNetworking/AFNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworking
+//
+//  Created by Kevin on 15/3/25.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetworking/AFNetworking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworking
+//
+//  Created by Kevin on 15/3/25.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
